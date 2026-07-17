0e384c2c6cb98734747a7860208e8ecdbfaac51a
ziwenwong <ziwenwong0130@gmail.com>
2026-07-17 16:10:08 +0800

    Delete printking directory

--- printking/api/admin/logout.js
+++
@@ -1,8 +0,0 @@
-const { clearSessionCookie } = require("../_lib/auth");
-const { sendJson } = require("../_lib/http");
-
-module.exports = async function handler(req, res) {
-  if (req.method !== "POST") return sendJson(res, 405, { error: "Method not allowed" });
-  clearSessionCookie(res);
-  return sendJson(res, 200, { ok: true });
-};
